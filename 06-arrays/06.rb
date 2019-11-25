6b31d069f437abf58243cd08bb747d4a53aeb90e
Brian Simmons <hello.brian.simmons@gmail.com>
2019-11-25 17:03:32 -0600

    Chapter 6

---
+++ 06-arrays/06.rb
@@ -0,0 +1,7 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names['margaret'] = 'jody'
+
+# The key 'margaret' is a string and needs to be an integer. A way to fix it
+# would be:
+
+names[3] = 'jody'
